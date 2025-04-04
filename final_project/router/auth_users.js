0e6789707e8e32efb74f33993e64f11057d6fbe2
sujithvp <sujithvpanicker@gmail.com>
2025-04-04 13:21:59 -0400

    third commit

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -8,6 +8,16 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+
+    let validuser = users.filter((user) => {
+        return (user.username === username);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validuser.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
