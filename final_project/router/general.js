d76046c7aa4bdeab3f7cfc84d17d45f0e11e9644
sujithvp <sujithvpanicker@gmail.com>
2025-04-04 13:17:39 -0400

    second commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,6 +5,19 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 public_users.post("/register", (req,res) => {
   //Write your code here
     const username = req.body.username;
@@ -18,7 +31,7 @@ public_users.post("/register", (req,res) => {
     // Check if both username and password are provided
     if (username && password) {
         // Check if the user does not already exist
-        if (!authenticatedUser(username)) {
+        if (!doesExist(username)) {
             // Add the new user to the users array
             users.push({"username": username, "password": password});
             return res.status(200).json({message: "User successfully registered. Now you can login"});
